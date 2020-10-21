240b5a03d22f049ff9388d44975a7fe6f2eb20f2
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-10-21 23:12:34 +0300

    crate hero section

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Button } from '../ButtonElement';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from './HeroElements';
+
+import Video from '../../videos/video.mp4';
+
+const HeroSection = () => {
+  const [hover, setHover] = useState(false);
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Virtual Banking Made Easy</HeroH1>
+        <HeroP>
+          Sign up for a new account today and recive $250 in credit towards your
+          next payment.
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='signup'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary='true'
+            dark='true'
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default HeroSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
 
@@ -13,6 +14,7 @@ const Home = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle}/>
       <Navbar toggle={toggle}/>
+      <HeroSection />
     </>
   )
 }
