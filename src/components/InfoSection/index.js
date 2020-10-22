74120d3bd0457da0e2f64ffebec328d359aed6a0
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-10-22 21:05:06 +0300

    create three sections

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -16,26 +16,53 @@ import {
 
 import { Button } from '../../components/ButtonElement';
 
-const InfoSection = () => {
+const InfoSection = ({
+  id,
+  lightBg,
+  imgStart,
+  topLine,
+  lightText,
+  headLine,
+  darkText,
+  description,
+  buttonLabel,
+  img,
+  alt,
+  primary,
+  dark,
+  dark2
+}) => {
   return (
     <>
-      <InfoContainer>
+      <InfoContainer id={id} lightBg={lightBg}>
         <InfoWrapper>
-          <InfoRow>
+          <InfoRow imgStart={imgStart}>
             <Column1>
               <TextWrapper>
-                <TopLine>TopLine</TopLine>
-                <Heading>Heading</Heading>
-                <Subtitle>Subtitle</Subtitle>
+                <TopLine>{topLine}</TopLine>
+                <Heading lightText={lightText}>{headLine}</Heading>
+                <Subtitle darkText={darkText}>{description}</Subtitle>
                 <BtnWrap>
-                  <Button to='home'>Button</Button>
+                  <Button
+                    to='home'
+                    smooth={true}
+                    duration={500}
+                    spy={true}
+                    exact='true'
+                    offset={-80}
+                    primary={primary ? 1 : 0}
+                    dark={dark ? 1 : 0}
+                    dark2={dark2 ? 1 : 0}
+                  >
+                    {buttonLabel}
+                  </Button>
                 </BtnWrap>
               </TextWrapper>
             </Column1>
 
             <Column2>
               <ImgWrap>
-                <Img />
+                <Img src={img} alt={alt} />
               </ImgWrap>
             </Column2>
           </InfoRow>
