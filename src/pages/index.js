1c68278a5711a0bfbb547119bb34fdf18e4565c4
Alexey Rumyantsev <alekseyrumyancev@mail.ru>
2020-10-22 18:50:18 +0300

    create InfoElements

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import HeroSection from '../components/HeroSection'
+import InfoSection from '../components/InfoSection'
 
 const Home = () => {
 
@@ -15,6 +16,7 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle}/>
       <Navbar toggle={toggle}/>
       <HeroSection />
+      <InfoSection />
     </>
   )
 }
